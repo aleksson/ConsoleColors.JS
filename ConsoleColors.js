1428a749ee106d76b5aff031aa319ebdd6cd1581
a.Cincarevic <acoaco908@gmail.com>
2016-11-16 06:21:39 +0100

    added examples

--- ConsoleColors.js
+++ ConsoleColors.js
@@ -1,12 +1,50 @@
-color = {};
-color.log = function (text,color) {
-	text = '%c ' + text;
-	color = 'color:'+ color +';';
-	return console.log(text,color);
+var log = console.log;
+console = {};
+
+console.log = function (text,color,background,border,borderColor) {
+	text = '%c ' + text +' ';
+	style = 'color:'+ color +';';
+    if(background) style += 'background:'+background+';';
+    if(border) style += 'border: 2px solid black;';
+    if(borderColor) style += 'border: 2px solid '+ borderColor+';';
+	return log(text,style);
 }
 
-color.log("ConsoleColors.JS","gray");
-color.log("i.","red");
-color.log("n..","orange");
-color.log("i...","green");
-color.log("t....","blue");
+
+console.ok = function (text) {
+    text = '%c ' + text+' ';
+    style = 'color:white;';
+    style += 'background:green;';
+    return log(text,style);
+}
+
+console.warn = function (text) {
+    text = '%c ' + text +' ';
+    style = 'color:white;';
+    style += 'background:red;';
+    return log(text,style);
+}
+
+console.error = function (text) {
+    text = '%c ' + text +' ';
+    style = 'color:white;';
+    style += 'background:orange;';
+    return log(text,style);
+}
+
+
+
+log("ConsoleColors Examples:")
+console.log("console with text colors","darkred");
+console.log("console with background colors","black","orange");
+console.log("console with border","cyan","green",true);
+console.log("and border colors","cyan","green",true,"purple");
+log("\n\n\n\n");
+
+
+log("ConsoleColors builtIn Components");
+log("----------->")
+console.ok("console.ok");
+console.warn("console.warn");
+console.error("console.error");
+log("<-----------");
