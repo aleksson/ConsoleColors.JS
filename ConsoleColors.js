a3ba31e5f1aa1b203d77614fb2cc2ec9029213c5
ACE/ <16833626+aleksson@users.noreply.github.com>
2024-05-08 16:21:25 +0200

    new-updates
    changed example, anf recatoring

--- ConsoleColors.js
+++ ConsoleColors.js
@@ -1,50 +1,52 @@
-var log = console.log;
+let log = console.log;
 console = {};
 
-console.log = function (text,color,background,border,borderColor) {
-	text = '%c ' + text +' ';
-	style = 'color:'+ color +';';
-    if(background) style += 'background:'+background+';';
-    if(border) style += 'border: 2px solid black;';
-    if(borderColor) style += 'border: 2px solid '+ borderColor+';';
+
+console.log = (text, textColor, backgroundColor, border, borderColor) => {
+	text = `%c${text}`;
+	style = `color:${textColor};`;
+    if(backgroundColor) style += `background:${backgroundColor};`;
+    if(border) style += `border: 2px solid black;`;
+    if(borderColor) style += `border: 2px solid ${borderColor};`;
+
 	return log(text,style);
 }
 
+console.ok = (text) => {
+    text = `%c ${text} `;
+    style = 'color: white;';
+    style += 'background: green;';
 
-console.ok = function (text) {
-    text = '%c ' + text+' ';
-    style = 'color:white;';
-    style += 'background:green;';
     return log(text,style);
 }
 
-console.warn = function (text) {
-    text = '%c ' + text +' ';
-    style = 'color:white;';
-    style += 'background:orange;';
+console.warn = (text) => {
+    text = `%c ${text} `;
+    style = 'color: white;';
+    style += 'background: darkorange;';
+
     return log(text,style);
 }
 
-console.error = function (text) {
-    text = '%c ' + text +' ';
-    style = 'color:white;';
-    style += 'background:red;';
-    return log(text,style);
+console.error = (text) => {
+    text = `%c ${text} `;
+    style = 'color :white;';
+    style += 'background: red;';
+
+    log(text,style);
 }
 
-
-
-log("ConsoleColors Examples:")
-console.log("console with text colors","darkred");
-console.log("console with background colors","black","orange");
-console.log("console with border","cyan","green",true);
-console.log("and border colors","cyan","green",true,"purple");
-log("\n\n\n\n");
-
-
-log("ConsoleColors builtIn Components");
-log("----------->")
-console.ok("console.ok");
-console.warn("console.warn");
-console.error("console.error");
-log("<-----------");
+const showDemo = () => {
+    console.log("ConsoleColors Examples:");
+    console.log("Text color", "orange");
+    console.log("Background color", "black", "orange");
+    console.log("Text with border", "white", "green", true);
+    console.log("Text Color with border Color", "white", "green", true, "orange");
+    console.log("\n\n\n\n");
+    
+    log("Usefull Color Methods");
+    console.ok("console.ok");
+    console.warn("console.warn");
+    console.error("console.error");
+}
+//showDemo();
